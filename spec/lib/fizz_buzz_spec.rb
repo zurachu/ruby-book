d4c9210b415df5f19270dd6725a21b67bb1c0232
zurachu <zurachu@gmail.com>
2018-01-08 10:29:47 +0900

    3.4 Column RSpec でテストコードを書く

---
+++ spec/lib/fizz_buzz_spec.rb
@@ -0,0 +1,13 @@
+require './lib/fizz_buzz'
+
+RSpec.describe 'Fizz Buzz' do
+  example 'fizz_buzz' do
+    expect(fizz_buzz(1)).to eq '1'
+    expect(fizz_buzz(2)).to eq '2'
+    expect(fizz_buzz(3)).to eq 'Fizz'
+    expect(fizz_buzz(4)).to eq '4'
+    expect(fizz_buzz(5)).to eq 'Buzz'
+    expect(fizz_buzz(6)).to eq 'Fizz'
+    expect(fizz_buzz(15)).to eq 'Fizz Buzz'
+  end
+end
